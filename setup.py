1c2a80496eee9692288f1f22d24efbccbe11d2ef
Thomas Holder <tholder7@gatech.edu>
2022-09-21 10:54:39 -0400

    Filled out minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='EosLib',
+      version='0.0',
+      description='Library of shared code between EosPayload and EosGround',
+      author='Lightning From The Edge of Space',
+      author_email='tholder7@gatech.edu',
+      packages=['EosLib'],
+      )
