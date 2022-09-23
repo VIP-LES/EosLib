940554537f69a3814446a49468ada918b33042c9
Thomas Holder <tholder7@gatech.edu>
2022-09-23 09:04:41 -0400

    Ensure enum value uniqueness

--- EosLib/packet/definitions.py
+++ EosLib/packet/definitions.py
@@ -1,15 +1,22 @@
-from enum import Enum
+from enum import Enum, unique
 
 
+@unique
 class PacketType(Enum):
     TELEMETRY = 0,
     WARNING = 1,
     ERROR = 2
 
 
+@unique
 class Priority(Enum):
     NO_TRANSMIT = -1,
     URGENT = 1,
     TELEMETRY = 2,
     DATA = 10
 
+
+@unique
+class Device(Enum):
+    ALTIMETER = 1,
+    GPS = 2,
