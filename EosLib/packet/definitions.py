243dccb350ee16ffa2065b3cc9b9338fada6da05
Thomas Holder <tholder7@gatech.edu>
2022-10-31 16:27:50 -0400

    Expand PacketDevice list

--- EosLib/packet/definitions.py
+++ EosLib/packet/definitions.py
@@ -5,23 +5,50 @@ RADIO_MAX_BYTES = 255
 
 @unique
 class PacketType(IntEnum):
-    TELEMETRY = 0,
-    WARNING = 1,
-    ERROR = 2
+    TELEMETRY = 0
+    WARNING = 1
+    DATA = 2
+    ERROR = 255
 
 
 @unique
 class PacketPriority(IntEnum):
-    NO_TRANSMIT = 255,
-    URGENT = 1,
-    TELEMETRY = 2,
+    NO_TRANSMIT = 255
+    URGENT = 1
+    TELEMETRY = 2
     DATA = 10
 
 
 @unique
 class PacketDevice(IntEnum):
-    ALTIMETER = 1,
-    GPS = 2,
+    TEMPERATURE_HUMIDITY = 0
+    PRESSURE = 1
+    PARTICULATES = 2
+    IR_VISIBLE_LIGHT = 3
+    VISIBLE_UVA_LIGHT = 4
+    UVA_UVB_LIGHT = 5
+    CO2 = 6
+    O3 = 7
+    MISC_SENSOR_1 = 8
+    MISC_SENSOR_2 = 9
+    MISC_SENSOR_3 = 10
+    MISC_SENSOR_4 = 11
+    REEFING_MOTOR = 12
+    MISC_ENGINEERING_1 = 13
+    MISC_ENGINEERING_2 = 14
+    CAMERA_1 = 15
+    CAMERA_2 = 16
+    MISC_CAMERA_1 = 17
+    MISC_CAMERA_2 = 18
+    RADIO = 19
+    MISC_RADIO_1 = 20
+    MISC_RADIO_2 = 21
+    GPS = 22
+    IMU = 23
+    MISC_1 = 24
+    MISC_2 = 25
+    MISC_3 = 26
+    MISC_4 = 27
 
 
 @unique
