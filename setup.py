5312a59b6943a8d9ac9ff399122d4b3f3c801511
Thomas Holder <tholder7@gatech.edu>
2022-11-14 14:02:28 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='EosLib',
-      version='0.0',
+      version='0.2.0',
       description='Library of shared code between EosPayload and EosGround',
       author='Lightning From The Edge of Space',
       author_email='tholder7@gatech.edu',
