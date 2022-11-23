4fc4fe6d85b1d378a2fc60082b3d0286c13bb197
Thomas Holder <tholder7@gatech.edu>
2022-11-23 12:42:52 -0500

    Add test of minimal constructor

--- tests/packet/test_packet.py
+++ tests/packet/test_packet.py
@@ -1,4 +1,6 @@
 import pytest
+
+import EosLib
 import EosLib.packet.definitions as definitions
 
 from datetime import datetime
@@ -15,6 +17,12 @@ def get_valid_packet():
                              datetime.now())
 
     return Packet(bytes("Hello World", 'utf-8'), data_header, transmit_header)
+
+
+def test_minimal_constructor():
+    data_header = DataHeader(EosLib.Device.GPS)
+    packet = Packet(b'Hello, World', data_header)
+    packet.encode()
 
 
 def test_validate_good_transmit_header():
