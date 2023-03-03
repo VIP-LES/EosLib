a9ee2e30b5f19b913f838f6e39509e58269a70b9
Ion Li <fuzhou.ion@gmail.com>
2023-03-03 13:05:12 -0500

    moved telemetryData test to separate file

--- tests/packet/test_packet.py
+++ tests/packet/test_packet.py
@@ -285,11 +285,3 @@ def test_packet_print_no_body(packet):
                       "No body"
 
     assert expected_string == packet.__str__()
-
-
-def test_telemetry_data():
-    current_time = datetime.now().timestamp()
-    new_data = TelemetryData(current_time, 0, 0, 0, 0, 0, 0)
-    valid = new_data.set_validity()
-    encoded_new_data = new_data.encode_data()
-    decoded_new_data = TelemetryData.decode_data(encoded_new_data)
