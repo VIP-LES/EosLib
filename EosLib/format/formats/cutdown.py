41a6dcf3afbaef105d206c1b9f0e4d243ffca5b7
Ion Li <fuzhou.ion@gmail.com>
2023-10-02 16:12:07 -0400

    gave cutdown its own enum type

--- EosLib/format/formats/cutdown.py
+++ EosLib/format/formats/cutdown.py
@@ -7,7 +7,7 @@ from EosLib.format.base_format import BaseFormat
 class CutDown(BaseFormat):
     @staticmethod
     def get_format_type() -> Type:
-        return Type.COMMAND
+        return Type.CUTDOWN
 
     @staticmethod
     def get_format_string() -> str:
