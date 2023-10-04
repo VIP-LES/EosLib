0db62edafb498ba2f61cb1bd1ea0bb760c077b08
VanL15 <94021265+VanL15@users.noreply.github.com>
2023-10-04 12:03:38 -0400

    Change command/format types to be more generic

--- EosLib/format/definitions.py
+++ EosLib/format/definitions.py
@@ -1,18 +1,24 @@
 from enum import unique, IntEnum
 
 
-@unique
 class Type(IntEnum):
     NO_TYPE = 0
     TELEMETRY = 1
     WARNING = 2
     DATA = 3
     POSITION = 4
-    COMMAND = 5
-    TELEMETRY_DATA = 7
-    EMPTY = 8
+    TELEMETRY_DATA = 5
+    EMPTY = 6
     RESPONSE_START = 32
-    PING_RESPONSE = 33
-    CUTDOWN_RESPONSE = 34
-    RESPONSE_END = 35
+    RESPONSE_1 = 33
+    RESPONSE_2 = 34
+    RESPONSE_3 = 35
+    RESPONSE_4 = 36
+    RESPONSE_END = 37
+    COMMAND_START = 32
+    COMMAND_1 = 40
+    COMMAND_2 = 41
+    COMMAND_3 = 42
+    COMMAND_4 = 43
+    COMMAND_END = 44
     ERROR = 255
