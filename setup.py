4da2106ab83e668fbe935c8fc1e695648bf8d1de
VanL15 <94021265+VanL15@users.noreply.github.com>
2023-11-09 21:43:57 -0500

    bump version to 4.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='EosLib',
-      version='4.3.0',
+      version='4.3.1',
       description='Library of shared code between EosPayload and EosGround',
       author='Lightning From The Edge of Space',
       author_email='thomasmholder@gmail.com',
