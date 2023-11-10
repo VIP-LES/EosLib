c5a533622ecb28d807c6f8d1647e6b8b9acc012c
VanL15 <94021265+VanL15@users.noreply.github.com>
2023-11-10 16:46:57 -0500

    change bytearray to bytes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='EosLib',
-      version='4.3.3',
+      version='4.3.4',
       description='Library of shared code between EosPayload and EosGround',
       author='Lightning From The Edge of Space',
       author_email='thomasmholder@gmail.com',
