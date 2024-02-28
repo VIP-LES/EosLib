2a4190cd47b429ea40a3b53e8e0bc31cc5d93584
VanL15 <94021265+VanL15@users.noreply.github.com>
2024-02-28 12:56:48 -0500

    add formats to init.py

--- EosLib/format/__init__.py
+++ EosLib/format/__init__.py
@@ -1,4 +1,4 @@
 from EosLib.format.formats import telemetry_data, position, empty_format, cutdown, ping_format, valve, e_field, \
-                                  science_data
+                                  science_data, downlink_chunk_format, downlink_header_format
 from EosLib.format.formats.health import driver_health_report
 from EosLib.format.definitions import Type as Type
