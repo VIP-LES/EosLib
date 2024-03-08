211261a14698129412da9f63151dbbf9403bb215
Panya Bhinder <pgbhinder@gmail.com>
2024-03-08 12:11:52 -0500

    107 added more descriptive error to packet decode

--- EosLib/packet/packet.py
+++ EosLib/packet/packet.py
@@ -141,6 +141,7 @@ class Packet:
             decoded_data_header = decoded_data_header
             packet_bytes = packet_bytes[struct.calcsize(DataHeader.data_header_struct_format_string):]
         else:
+            print(f"Unexpected packet header: {packet_bytes[0]}, should be: {HeaderPreamble.DATA}")
             raise PacketFormatError("Packet does not contain a header")
 
         decoded_packet = Packet(EosLib.format.decode_factory.decode_factory.decode(decoded_data_header.data_type,
